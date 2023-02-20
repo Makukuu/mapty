1e896461d51816658bd8ecc0a67a2041db20ec27
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-02-20 23:34:22 +0100

    Refactor
    Move functions in the App class methods

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 This is a follow along project from the course:
 https://www.udemy.com/course/the-complete-javascript-course/
 
+Detailed notes were made in the form of comments for future reference.
+
 Through this project the following subjects were covered and learned:
 
 - Project planning
--- script.js
+++ script.js
@@ -11,76 +11,100 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-// making map a global variable
-let map, mapEvent;
+class App {
+  // Private instance properties
+  #map;
+  #mapEvent;
 
-// check if it exist because of old browsers
-if (navigator.geolocation)
-  // arguments are 2 callback functions:
-  // 1st gets called on success, the argument is called the Position Parameter
-  // 2nd gets called on failure
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  constructor() {
+    this._getPosition();
 
-      const coords = [latitude, longitude];
+    // the submit event gets triggered also when we hit ENTER
+    // the 'this' keyword inside the _newWorkout will point to form, this is fixed by using bind()
+    form.addEventListener("submit", this._newWorkout.bind(this));
 
-      // the "map" in parenthessis corresponds to an id tag in our html, that's where the map will be displayed
-      // 2nd argument of setView is the zoom level
-      map = L.map("map").setView(coords, 13);
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
 
-      //we can set different styles of openstreetmap
-      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  _getPosition() {
+    // check if it exist because of old browsers
+    if (navigator.geolocation)
+      // arguments are 2 callback functions:
+      // 1st gets called on success, the argument is called the Position Parameter
+      // 2nd gets called on failure
 
-      // this is basically an event listener for the map
-      // Handling clicks on map
-      map.on("click", function (mapE) {
-        // copying to a global variable because we need it at form submit event
-        mapEvent = mapE;
-        form.classList.remove("hidden");
-        // good for user experience, we can immediately start typing
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert("Could not get your position!");
-    }
-  );
+      //!! getCurrentPosition will call the loadMap as a regular function call and thus the this keyword will be undefined!
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Could not get your position!");
+        }
+      );
+  }
 
-// the submit event gets triggered also when we hit ENTER
-form.addEventListener("submit", function (e) {
-  // to stop it from appearing and reloading
-  e.preventDefault();
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-  // Clear input fields
-  // inputDistance.value = inputDuration.value = inputElevation.value = "";
+    const coords = [latitude, longitude];
 
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
+    // the "map" in parenthessis corresponds to an id tag in our html, that's where the map will be displayed
+    // 2nd argument of setView is the zoom level
+    this.#map = L.map("map").setView(coords, 13);
 
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    .setPopupContent("workout")
-    .openPopup();
-});
+    //we can set different styles of openstreetmap
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-// changing form options for different workout types (running vs cycling)
-inputType.addEventListener("change", function () {
-  // closest() selects parents
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
+    // this is basically an event listener for the map
+    // Handling clicks on map
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    // copying to a global variable because we need it at form submit event
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    // good for user experience, we can immediately start typing
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    // closest() selects parents
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  _newWorkout(e) {
+    // to stop it from appearing and reloading
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value = inputDuration.value = inputElevation.value = "";
+
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("workout")
+      .openPopup();
+
+    // changing form options for different workout types (running vs cycling)
+    inputType.addEventListener("change", function () {});
+  }
+}
+
+// main
+const app = new App();

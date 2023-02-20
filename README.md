57544248d1496ff18df4078fa4f1431f054beab7
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-02-20 18:10:14 +0100

    Add leaflet library and display current location

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Through this project the following subjects were covered and learned:
 
 - Project planning
 - geolocation API
+- Leaflet library
--- script.js
+++ script.js
@@ -21,6 +21,23 @@ if (navigator.geolocation)
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      // the "map" in parenthessis corresponds to an id tag in our html, that's where the map will be displayed
+      // 2nd argument of setView is the zoom level
+      const map = L.map("map").setView(coords, 13);
+
+      //we can set different styles of openstreetmap
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
     },
     function () {
       alert("Could not get your position!");

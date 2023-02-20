042206e1a9d2da8e30ca4f25f3f8b67bbcee7826
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-02-20 19:59:44 +0100

    Render workout input form

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+// making map a global variable
+let map, mapEvent;
+
 // check if it exist because of old browsers
 if (navigator.geolocation)
   // arguments are 2 callback functions:
@@ -26,7 +29,7 @@ if (navigator.geolocation)
 
       // the "map" in parenthessis corresponds to an id tag in our html, that's where the map will be displayed
       // 2nd argument of setView is the zoom level
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       //we can set different styles of openstreetmap
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
@@ -35,26 +38,49 @@ if (navigator.geolocation)
       }).addTo(map);
 
       // this is basically an event listener for the map
-      map.on("click", function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: "running-popup",
-            })
-          )
-          .setPopupContent("workout")
-          .openPopup();
+      // Handling clicks on map
+      map.on("click", function (mapE) {
+        // copying to a global variable because we need it at form submit event
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        // good for user experience, we can immediately start typing
+        inputDistance.focus();
       });
     },
     function () {
       alert("Could not get your position!");
     }
   );
+
+// the submit event gets triggered also when we hit ENTER
+form.addEventListener("submit", function (e) {
+  // to stop it from appearing and reloading
+  e.preventDefault();
+
+  // Clear input fields
+  // inputDistance.value = inputDuration.value = inputElevation.value = "";
+
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("workout")
+    .openPopup();
+});
+
+// changing form options for different workout types (running vs cycling)
+inputType.addEventListener("change", function () {
+  // closest() selects parents
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});

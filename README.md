015bbfb4491b887abc62f3b6ee7b34db13b27731
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-02-20 17:47:33 +0100

    Display current location
    Use the geolocation API to display your current location

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 This is a follow along project from the course:
 https://www.udemy.com/course/the-complete-javascript-course/
 
-## Through this project the following subjects were covered and learned:
+Through this project the following subjects were covered and learned:
+
+- Project planning
+- geolocation API
--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+// check if it exist because of old browsers
+if (navigator.geolocation)
+  // arguments are 2 callback functions:
+  // 1st gets called on success, the argument is called the Position Parameter
+  // 2nd gets called on failure
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert("Could not get your position!");
+    }
+  );
